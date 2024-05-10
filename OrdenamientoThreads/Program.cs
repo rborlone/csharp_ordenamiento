17a40f47477291732ac6ac2933f820dc5fd2759c
Rodrigo Borlone Espoz <rodrigo.borlone@gmail.com>
2024-05-10 15:44:17 -0400

    Se finaliza ordenamiento correctamente

--- OrdenamientoThreads/Program.cs
+++ OrdenamientoThreads/Program.cs
@@ -1,68 +1,104 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
-public class Program{
-    static public int Main(){
-        List<int> numeros = Enumerable.Range(0, 5).ToList();
-         
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+class Program
+{
+    static void MostrarLista(List<int> numeros){
+        foreach (int num in numeros)
+            Console.Write(string.Format("{0}, ", num));
+        Console.WriteLine("\n");
+    }
+
+    static List<int> mergeSort(List<int> num, int a){
+        
+        List<int> resultado = new List<int>(num.Count);
+        int max=num.Count;
+        int j = a;
+        int n = 0;
+        int x = 0;
+
+        for (int i = 0; i<num.Count; i++){
+            if (x == a)
+                resultado.Add(num[j++]);
+            else if (j == max)
+                resultado.Add(num[x++]);
+            else if (num[x] < num[j])
+                resultado.Add(num[x++]);
+            else
+                resultado.Add(num[j++]);
+            n++;
+        }
+        
+        return resultado;
+    }
+
+    static List<int> bubbleSort(List<int> num){
+        for(int i=0; i<num.Count; i++){
+            for(int j=0; j<num.Count; j++){
+                if (num[i]<num[j]){
+                    int cont = num[j];
+                    num[j] = num[i];
+                    num[i] = cont;
+                }
+            }
+        }
+
+        return num;
+    }
+
+    static void Revolver<T>(List<T> list, int inicio, int fin, Random rand)
+    {
+        int n = fin - inicio;
+        while (n > 1){
+            int k = rand.Next(n--);
+            T temp = list[inicio + k];
+            list[inicio + k] = list[inicio + n];
+            list[inicio + n] = temp;
+        }
+    }
+
+    static void Main(string[] args){
         Random seeder = new Random();
         int seed = seeder.Next();
-        Random engine = new Random(seed);
+        Random rand = new Random(seed);
+        
+        List<int> numeros = Enumerable.Range(1, 20).ToList();
 
-        //ciudades = new List<int>(){0,48,31,44,18,40,7,8,9,42,32,50,10,51,13,12,46,25,26,27,11,24,3,5,14,4,23,47,37,36,39,38,35,34,33,43,45,15,28,49,19,22,29,1,6,41,20,16,2,17,30,21,0};
-        Revolver(numeros, 0, numeros.Count - 1, engine);
+        Revolver(numeros, 0, numeros.Count - 1, rand);
 
-        foreach(int i in numeros){
-            Console.Write(string.Format("{0},", i));
-        }
-
-        Ordenar(numeros);
-
-        Console.WriteLine("");
-
-        foreach(int i in numeros){
-            Console.Write(string.Format("{0},", i));
-        }
-
+        //Mostramos la lista
+        MostrarLista(numeros);
 
         int dividir = numeros.Count / 2;
 
         List<int> num1 = numeros.GetRange(0, dividir);
-        List<int> num2 = numeros.GetRange(dividir + 1.env, numeros.Count);
+        List<int> num2 = numeros.GetRange(dividir, numeros.Count - dividir);
+    
+        List<Thread> threads = new List<Thread>();
 
+        // threads.Add(new Thread(() => num1.Sort(0, num1.Count, null)));
+        // threads.Add(new Thread(() => num2.Sort(0, num2.Count, null)));
 
+        threads.Add(new Thread(() => bubbleSort(num1)));
+        threads.Add(new Thread(() => bubbleSort(num2)));
 
-
-
-
-
-        return 1;
-    }
-
-    public static void Ordenar(List<int> a){
-        for(int i=0; i<a.Count; i++){
-            for(int j=1; j<a.Count; j++){
-                if (a[i]<a[j]){
-                    int cont = a[j];
-                    a[j] = a[i];
-                    a[i] = cont;
-                }
-            }
+        foreach (var thread in threads) {
+            thread.Start();
         }
-    }
 
-        /***
-        Metodo para barajar las ciudades.
-    */
-    private static void Revolver<T>(List<T> list, int start, int end, Random rng)
-    {
-        int n = end - start;
-        while (n > 1)
-        {
-            int k = rng.Next(n--);
-            T temp = list[start + k];
-            list[start + k] = list[start + n];
-            list[start + n] = temp;
+        foreach (var thread in threads) {
+            thread.Join();
         }
-    }
+        
+        num1.AddRange(num2);
+        numeros = num1;
 
+        MostrarLista(numeros);
+
+        numeros = mergeSort(numeros, dividir);
+
+        MostrarLista(numeros);
+    }
 }
